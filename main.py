45e300f57c1cb0fd26dc3a4e2d08f2f81a4e5a38
nikste <nikolaas.steenbergen@gmail.com>
2017-04-10 23:18:17 +0200

    Add segmentation network, .gitignore

--- main.py
+++ main.py
@@ -16,24 +16,16 @@ with open("params/params.yaml", 'r') as stream:
 
 img_size_x = 16
 img_size_y = 16
-batch_size = 10
+batch_size = 1000
 max_iter = 10000
-dg = GenerateRedRects(size=[img_size_x,img_size_y,3], batch_size=batch_size)
-layer_info=[{'name': 'c1', 'height': img_size_x, 'width': img_size_y, 'input': 3, 'output': 64},
+dg = GenerateRedRects(size=[img_size_x,img_size_y, 3], batch_size=batch_size)
+layer_info = [{'name': 'c1', 'height': img_size_x, 'width': img_size_y, 'input': 3, 'output': 64},
             {'name': 'c2', 'height': img_size_x, 'width': img_size_y, 'input': 64, 'output': 128}]
-ssn = SimpleSegmentationNetwork(layer_info=layer_info)
 
-# x, y, y_seg = dg.next()
+print "cfg['CHECKPOINT_DIR']", cfg['CHECKPOINT_DIR']
 
-# for i in range(0, batch_size):
-#     plt.imshow(x[i])
-#     plt.imshow(y_seg[i])
-#     plt.show()
-
-init_op = tf.global_variables_initializer()
 sess = tf.Session()
-
-sess.run(init_op)
+ssn = SimpleSegmentationNetwork(sess, checkpoint_dir=cfg['CHECKPOINT_DIR'], load_from_checkpoint=cfg['LOAD_FROM_CHECKPOINT'], num_classes=2, layer_info=layer_info)
 
 # convenience
 train_step = ssn.train_step
@@ -42,52 +34,37 @@ x = ssn.x
 y_ = ssn.y_
 output = ssn.output
 y_ = ssn.y_
-loss = ssn.loss
+loss = ssn.cross_entropy
 netout = ssn.raw_scores_per_pxl
-
-accuracy, loss, netout
-
-tf.summary.scalar('loss', loss)
-tf.summary.scalar('accuracy', accuracy)
-nn = tf.split(netout, 2, axis=3)
-tf.summary.image('netout', nn[0])
-tf.summary.image('netout', nn[1])
-
-tf.summary.image('input', x)
-# tf.summary.image('gt', y_)
-graph = tf.get_default_graph()
-init_op = tf.global_variables_initializer()
-sess.run(init_op)
-
+summary_op = ssn.summary_op
 summary_writer = tf.summary.FileWriter("./summary/" + str(datetime.datetime.now()) + "/", sess.graph)
-run_metadata = tf.RunMetadata()
 
-merged = tf.summary.merge_all()
+global_step = ssn.global_step
 
 saver = tf.train.Saver()
-
-for i in range(0, max_iter):
+global_step_ = sess.run(global_step)
+for i in range(global_step_, max_iter):
     xx, yy, yy_seg, yy_seg_classes = dg.next()
-    _, summary, accuracy_, loss_, netout_ = sess.run([train_step, merged, accuracy, loss, netout], feed_dict={x: xx, y_: yy_seg_classes})
-    print i, "accuracy", accuracy_, "loss", loss_
-    summary_writer.add_summary(summary, global_step=i)
+    _, summary, accuracy_, loss_, netout_, global_step_ = sess.run([train_step, summary_op, accuracy, loss, netout, global_step], feed_dict={x: xx, y_: yy_seg_classes})
+    print i, "global_step", global_step_, "accuracy", accuracy_, "loss", loss_
+    summary_writer.add_summary(summary, global_step=global_step_)
 
-    if i % cfg['SAVE_INTERVAL'] == 0:
+    if i % cfg['SAVE_INTERVAL'] == 0 and cfg['SAVE_INTERVAL'] > 0:
         t_start_aux = datetime.datetime.now()
         save_path = saver.save(sess, cfg['CHECKPOINT_DIR'] + cfg['MODEL_NAME'], global_step=i)
         print "Model saved in file:", save_path, "done in", datetime.datetime.now() - t_start_aux
-    # if i % 5 == 0:
-    #     NUM_FIGS_X = 2
-    #     NUM_FIGS_Y = 2
-    #
-    #     fig = plt.figure()
-    #     a = fig.add_subplot(NUM_FIGS_X, NUM_FIGS_Y, 1)
-    #     plt.imshow(xx[0][:, :, 0])
-    #     a = fig.add_subplot(NUM_FIGS_X, NUM_FIGS_Y, 2)
-    #     plt.imshow(xx[0][:, :, 1])
-    #     a = fig.add_subplot(NUM_FIGS_X, NUM_FIGS_Y, 3)
-    #     plt.imshow(netout_[0][:, :, 0])
-    #     a = fig.add_subplot(NUM_FIGS_X, NUM_FIGS_Y, 4)
-    #     plt.imshow(netout_[0][:, :, 1])
-    #
-    #     plt.show()
+    if i % cfg['PLOT_RESULTS'] == 0 and cfg['PLOT_RESULTS'] > 0:
+        NUM_FIGS_X = 2
+        NUM_FIGS_Y = 2
+
+        fig = plt.figure()
+        a = fig.add_subplot(NUM_FIGS_X, NUM_FIGS_Y, 1)
+        plt.imshow(xx[0][:, :, 0])
+        a = fig.add_subplot(NUM_FIGS_X, NUM_FIGS_Y, 2)
+        plt.imshow(xx[0][:, :, 1])
+        a = fig.add_subplot(NUM_FIGS_X, NUM_FIGS_Y, 3)
+        plt.imshow(netout_[0][:, :, 0])
+        a = fig.add_subplot(NUM_FIGS_X, NUM_FIGS_Y, 4)
+        plt.imshow(netout_[0][:, :, 1])
+
+        plt.show()
--- net.py
+++ net.py
@@ -2,19 +2,52 @@ import tensorflow as tf
 from net_utils import convolutional, weight_variable
 
 
-class SimpleSegmentationNetwork():
-    def __init__(self, layer_info=[{'name': 'c1', 'height': 32, 'width': 32, 'input': 3, 'output': 64}], num_classes=2):
-        self.x = tf.placeholder(tf.float32)
-        self.y_ = tf.placeholder(tf.float32)
+class SimpleSegmentationNetwork:
 
-        self.layer_info = layer_info
-        self.layer = [self.x]
-        for i in range(0, len(layer_info)):
-            print i, 'len(self.layer)', len(self.layer), 'output', layer_info[i], 'input', self.layer[i - 1]
-            self.layer.append(self.create_hidden(layer_info[i], self.layer[i]))
+    def load_from_checkpoint(self, sess, checkpoint_dir):
+        file_path = tf.train.latest_checkpoint(checkpoint_dir=checkpoint_dir)
+        new_saver = tf.train.import_meta_graph(file_path + '.meta')
+        new_saver.restore(sess, file_path)
 
-        # self.output = self.layer[-1]
-        self.create_output(num_classes)
+        sess.run(tf.all_variables())
+        x = tf.get_collection('x')[0]
+        y_ = tf.get_collection('y_')[0]
+        raw_scores_per_pxl = tf.get_collection('raw_scores_per_pxl')[0]
+        output = tf.get_collection('output')[0]
+        accuracy = tf.get_collection('accuracy')[0]
+        global_step = tf.get_collection('global_step')[0]
+        train_step = tf.get_collection('train_step')[0]
+        cross_entropy = tf.get_collection('cross_entropy')[0]
+        summary_op = tf.get_collection('summary_op')[0]
+
+        return x, y_, raw_scores_per_pxl, output, accuracy, global_step, train_step, cross_entropy, summary_op
+
+    def __init__(self, sess, checkpoint_dir=None, load_from_checkpoint=False, num_classes=2,
+                 layer_info=[{'name': 'c1', 'height': 32, 'width': 32, 'input': 3, 'output': 64}]):
+
+        if load_from_checkpoint:
+            self.x, self.y_, self.raw_scores_per_pxl, self.output, \
+                self.accuracy, self.global_step, self.train_step, self.cross_entropy,\
+                self.summary_op = self.load_from_checkpoint(sess, checkpoint_dir=checkpoint_dir)
+        else:
+            self.x = tf.placeholder(tf.float32)
+            self.y_ = tf.placeholder(tf.float32)
+
+            self.layer_info = layer_info
+            self.layer = [self.x]
+            for i in range(0, len(layer_info)):
+                print i, 'len(self.layer)', len(self.layer), 'output', layer_info[i], 'input', self.layer[i - 1]
+                self.layer.append(self.create_hidden(layer_info[i], self.layer[i]))
+
+            # self.output = self.layer[-1]
+            self.raw_scores_per_pxl, self.output, \
+                self.accuracy, self.global_step, self.train_step, self.cross_entropy = self.create_output(num_classes)
+
+            self.summary_op = self.create_summaries()
+            self.create_collections()
+
+            init_op = tf.initialize_all_variables()
+            sess.run(init_op)
 
     def create_hidden(self, layer_info, layer_input):
         return convolutional(layer_input, self.layerinfo2filter(layer_info), name=layer_info['name'])
@@ -26,17 +59,45 @@ class SimpleSegmentationNetwork():
         out_conv = self.layer[-1]
         raw_scores_per_pxl = convolutional(out_conv, [self.layer_info[-1]['height'], self.layer_info[-1]['width'],
                                                       self.layer_info[-1]['output'], num_classes], name='raw_scores')
-        self.raw_scores_per_pxl = raw_scores_per_pxl
+        raw_scores_per_pxl = raw_scores_per_pxl
         raw_scores_per_pxl_flat = tf.reshape(raw_scores_per_pxl, [-1, num_classes])
         y_flat = tf.reshape(self.y_, [-1, num_classes])
-        cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_flat, logits=raw_scores_per_pxl_flat), name='cross_entropy')
-        self.loss = cross_entropy
 
+        # performance measures
+        output = tf.argmax(tf.nn.softmax(raw_scores_per_pxl), axis=-1)
+        accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(input=y_flat, axis=-1), tf.argmax(input=raw_scores_per_pxl_flat, axis=-1)), tf.float32))
 
-        self.accuracy = tf.reduce_mean(tf.cast(tf.equal(y_flat, raw_scores_per_pxl_flat), tf.float32))
-        self.train_step = tf.train.AdamOptimizer(1e-4).minimize(self.loss)
+        cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_flat, logits=raw_scores_per_pxl_flat),
+                                       name='cross_entropy')
 
-        self.output = tf.argmax(tf.nn.softmax(raw_scores_per_pxl), axis=-1)
+        # Train op
+        global_step = tf.Variable(0, name='global_step', trainable=False)
+        train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy, global_step=global_step)
+
+        return raw_scores_per_pxl, output, accuracy, global_step, train_step, cross_entropy
+
+    def create_summaries(self):
+        tf.summary.scalar('cross_entropy', self.cross_entropy)
+        tf.summary.scalar('accuracy', self.accuracy)
+        raw_scores_per_pxl_split = tf.split(self.raw_scores_per_pxl, 2, axis=3)
+        tf.summary.image('raw_scores_per_pxl0', raw_scores_per_pxl_split[0])
+        tf.summary.image('raw_scores_per_pxl1', raw_scores_per_pxl_split[1])
+        tf.summary.image('output', tf.expand_dims(tf.cast(self.output * 255, tf.uint8), axis=-1))
+        tf.summary.image('input', self.x)
+        run_metadata = tf.RunMetadata()
+        summary_op = tf.summary.merge_all()
+        return summary_op
+
+    def create_collections(self):
+        tf.add_to_collection('x', self.x)
+        tf.add_to_collection('y_', self.y_)
+        tf.add_to_collection('raw_scores_per_pxl', self.raw_scores_per_pxl)
+        tf.add_to_collection('output', self.output)
+        tf.add_to_collection('accuracy', self.accuracy)
+        tf.add_to_collection('global_step', self.global_step)
+        tf.add_to_collection('train_step', self.train_step)
+        tf.add_to_collection('cross_entropy', self.cross_entropy)
+        tf.add_to_collection('summary_op', self.summary_op)
 
     def create_output_ws(self, num_classes):
         out_conv = self.layer[-1]
